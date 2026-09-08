f2955190827b62e34a247ac0f5ecdae1aed7d9e5
alexandretihy <alex27.t27@gmail.com>
2026-09-08 17:19:42 +0200

    fonc puiss

--- TP1/fonctions.py
+++ TP1/fonctions.py
@@ -6,18 +6,40 @@ def puissance(a, b):
     return a**b
 
 def puissance(a, b):
-    if not type(a) is int or not type(b) is int:
-        raise TypeError("Les deux nombres doivent être des entiers")
+    if not type(a) is int or not type(b) is int:        raise TypeError("Les deux nombres doivent être des entiers")
     
     if a == 0:
         if b < 0:
             raise ValueError("0 à une puissance négative est indéfini")
         if b > 0:
             return 0
+      def puissance(a, b):
+
+    if type(a) != int or type(b) != int:
+        raise TypeError("Les deux nombres doivent être des entiers")
+    
+
+    if a == 0:
+        if b < 0:
+            raise ValueError("Puissance négative impossible pour 0")
+        if b > 0:
+            return 0
         return 1
 
-    return a**b
 
+    res = 1
+    
+
+    nb_iterations = b if b >= 0 else -b
+
+    for i in range(nb_iterations):
+        res = res * a
+
+
+    if b < 0:
+        return 1 / res
+
+    return res
 
 
 
