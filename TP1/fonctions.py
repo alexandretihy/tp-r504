e94e15da63e7405407f3092fc1e8b8392fe9852a
alexandretihy <alex27.t27@gmail.com>
2026-09-08 17:59:00 +0200

    modif3

--- TP1/fonctions.py
+++ TP1/fonctions.py
@@ -1,18 +1,29 @@
-def puissance (a, b):
-	is_int(a)
-	is_int(b)
-	result = 1
-	if b<0:
-		for i in range(-b):
-			result *= a 
-		if a == 0 :
-			raise ValueError("0 à une puissance négative est impossible")		
-		else : return (1/result)
+def is_int(var):
+    if type(var) != int:
+        raise TypeError("Les deux nombres doivent être des entiers")
 
-	for i in range(b):
-		result *= a
-	return (result)
+def puissance(a, b):
+    # 1. Vérification des types
+    is_int(a)
+    is_int(b)
 
-def is_int (var):
-	if type(var) != int:
-		raise TypeError("Only integers are allowed")
+    # 2. Cas particulier : base égale à 0
+    if a == 0:
+        if b < 0:
+            raise ValueError("0 à une puissance négative est impossible")
+        if b > 0:
+            return 0
+        return 1
+
+    # 3. Calcul par boucle for
+    res = 1
+    nb_iterations = b if b >= 0 else -b
+
+    for i in range(nb_iterations):
+        res *= a
+
+    # 4. Si l'exposant était négatif, on renvoie l'inverse
+    if b < 0:
+        return 1 / res
+
+    return res
