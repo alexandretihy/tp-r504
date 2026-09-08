df659e17dae4869fd54e52e5bfe1791bd8ac4dd4
alexandretihy <alex27.t27@gmail.com>
2026-09-08 17:12:23 +0200

    test auto

--- TP1/fonctions.py
+++ TP1/fonctions.py
@@ -5,6 +5,19 @@ def puissance(a, b):
            raise TypeError("Les deux nombres doivent être des entiers")
     return a**b
 
+def puissance(a, b):
+    if not type(a) is int or not type(b) is int:
+        raise TypeError("Les deux nombres doivent être des entiers")
+    
+    if a == 0:
+        if b < 0:
+            raise ValueError("0 à une puissance négative est indéfini")
+        if b > 0:
+            return 0
+        return 1
+
+    return a**b
+
 
 
 
--- TP1/test.py
+++ TP1/test.py
@@ -10,3 +10,7 @@ def test_2():
     assert f.puissance(-1, -1) == -1
     assert f.puissance(-1, -2) == 1
     assert f.puissance(-2, -1) == -0.5
+
+def test_3():	
+	with pytest.raises(ValueError):
+	   f.puissance(0, -3)
